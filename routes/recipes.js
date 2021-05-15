cf39c7424b181221d53f3d8961623cabec7daf0e
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-15 19:12:17 +0200

    recipes route

---
+++ routes/recipes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const recipesRouter = express_1.Router();
+recipesRouter.get('/', (req, res, next) => { });
+exports.default = recipesRouter;
---
+++ routes/recipes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const recipesRouter = Router();
+
+recipesRouter.get('/', (req, res, next) => {});
+
+export default recipesRouter;
