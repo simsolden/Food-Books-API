90c012ae7ea1d08517faa7c27da4530cc65c2009
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-17 14:57:00 +0200

    js files moved into dist folder

--- src/routes/recipes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const recipesRouter = express_1.Router();
-recipesRouter.get('/', (req, res, next) => { });
-exports.default = recipesRouter;
