6205fac97a9ffd1f2a66645a01a33c77443a9e98
Simon Oldenhove <simonoldenhove@gmail.com>
2021-05-27 18:16:33 +0200

    added planning update, remove fetch all plannings

--- src/controllers/planning.ts
+++ src/controllers/planning.ts
@@ -2,22 +2,6 @@ import HttpException from '../common/HttpException';
 import { Recipe } from '../models/recipe';
 import { UserPlanning } from '../models/userPlanning';
 
-export const createUserPlanning = async (req: any, res: any, next: any) => {
-  const userPlanning = new UserPlanning({
-    user: req.body.user,
-  });
-
-  req.body.planning && userPlanning.set('planning', req.body.planning);
-
-  try {
-    await UserPlanning.create(userPlanning);
-
-    res.status(200).json({ userPlanning });
-  } catch (err) {
-    res.status(500).json({ error: true, message: err.message });
-  }
-};
-
 export const addPlanningRecipe = async (req: any, res: any, next: any) => {
   const { user } = req;
 
@@ -50,6 +34,27 @@ export const findUserPlanning = async (req: any, res: any, next: any) => {
       const recipes = await Recipe.find().where('_id').in(ids).exec();
 
       res.status(200).json({ userPlanning, recipes });
+    } else {
+      throw new HttpException(404, 'planning not found');
+    }
+  } catch (err) {
+    res.status(err.status || 500).json({ message: err.message });
+  }
+};
+
+export const updatePlanning = async (req: any, res: any, next: any) => {
+  try {
+    const userId = req.user._id;
+    const { planning } = req.body;
+
+    const updatedPlanning = await UserPlanning.findOneAndUpdate({ user: userId }, { planning }, { new: true });
+
+    if (updatedPlanning) {
+      // @ts-ignore
+      const ids = updatedPlanning.planning.map((recipe) => recipe.recipeId);
+      const recipes = await Recipe.find().where('_id').in(ids).exec();
+
+      res.status(200).json({ userPlanning: updatedPlanning, recipes });
     } else {
       throw new HttpException(404, 'planning not found');
     }
