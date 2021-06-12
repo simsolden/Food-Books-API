73180ede8abf2539b460a379f0aec47ed0cbb4ab
Simon Oldenhove <simonoldenhove@gmail.com>
2021-06-12 17:19:04 +0200

    added rate limit to prevent bruteforce

---
+++ src/common/ExpressBrute.ts
@@ -0,0 +1,38 @@
+import ExpressBrute from 'express-brute';
+import MongooseStore from 'express-brute-mongoose';
+import BruteForceSchema from 'express-brute-mongoose/dist/schema';
+import mongoose from 'mongoose';
+
+const model = mongoose.model('bruteforce', new mongoose.Schema(BruteForceSchema));
+const store = new MongooseStore(model);
+
+const handleStoreError = (error: any) => {
+  console.error(error); // log this error so we can figure out what went wrong
+  // cause node to exit, hopefully restarting the process fixes the problem
+  throw {
+    message: error.message,
+    parent: error.parent,
+  };
+};
+
+const failCallback = function (req: any, res: any, next: any, nextValidRequestDate: any) {
+  res.json({ error: nextValidRequestDate, date: new Date() });
+};
+
+export const userBruteforce = new ExpressBrute(store, {
+  freeRetries: 5,
+  minWait: 5 * 60 * 1000, // 5 minutes
+  maxWait: 60 * 60 * 1000, // 1 hour,
+  failCallback: failCallback,
+  handleStoreError: handleStoreError,
+});
+
+export const globalBruteforce = new ExpressBrute(store, {
+  freeRetries: 1000,
+  attachResetToRequest: false,
+  minWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
+  maxWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
+  lifetime: 24 * 60 * 60, // 1 day (seconds not milliseconds)
+  failCallback: failCallback,
+  handleStoreError: handleStoreError,
+});
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,12 +2,23 @@ import { Router } from 'express';
 import { createUser, updateUser, login, findUserRecipes, autoLogin } from '../controllers/users';
 import { auth } from '../middlewares/authentication';
 import { pagination } from '../middlewares/users/pagination';
+import { userBruteforce, globalBruteforce } from '../common/ExpressBrute';
 
 const usersRouter = Router();
 
 usersRouter.post('/users', createUser);
 
-usersRouter.post('/login', login);
+usersRouter.post(
+  '/login',
+  globalBruteforce.prevent,
+  userBruteforce.getMiddleware({
+    key: (req, res, next) => {
+      // prevent too many attempts for the same username
+      next(req.body.email);
+    },
+  }),
+  login
+);
 
 usersRouter.get('/auto-login', auth, autoLogin);
 
