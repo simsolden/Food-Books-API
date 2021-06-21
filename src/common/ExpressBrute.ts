38d7682c9f854680748d2fd1b5f7c6f218fdc2a7
Simon Oldenhove <simonoldenhove@gmail.com>
2021-06-21 03:34:14 +0200

    bruteforce error message and nodemailer module

--- src/common/ExpressBrute.ts
+++ src/common/ExpressBrute.ts
@@ -2,6 +2,8 @@ import ExpressBrute from 'express-brute';
 import MongooseStore from 'express-brute-mongoose';
 import BruteForceSchema from 'express-brute-mongoose/dist/schema';
 import mongoose from 'mongoose';
+import moment from 'moment';
+import HttpException from './HttpException';
 
 const model = mongoose.model('bruteforce', new mongoose.Schema(BruteForceSchema));
 const store = new MongooseStore(model);
@@ -16,11 +18,12 @@ const handleStoreError = (error: any) => {
 };
 
 const failCallback = function (req: any, res: any, next: any, nextValidRequestDate: any) {
-  res.json({ error: nextValidRequestDate, date: new Date() });
+  moment.locale('fr');
+  return res.status(429).json({ error: true, message: `Veuillez rééssayer ${moment(nextValidRequestDate).fromNow()}` });
 };
 
 export const userBruteforce = new ExpressBrute(store, {
-  freeRetries: 5,
+  freeRetries: 3,
   minWait: 5 * 60 * 1000, // 5 minutes
   maxWait: 60 * 60 * 1000, // 1 hour,
   failCallback: failCallback,
@@ -28,7 +31,7 @@ export const userBruteforce = new ExpressBrute(store, {
 });
 
 export const globalBruteforce = new ExpressBrute(store, {
-  freeRetries: 1000,
+  freeRetries: 500,
   attachResetToRequest: false,
   minWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
   maxWait: 25 * 60 * 60 * 1000, // 1 day 1 hour (should never reach this wait time)
